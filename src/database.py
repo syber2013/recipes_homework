5692ab4318e68a3124ffcc83b8f2e103025abaeb
syber2013 <tralex2009@gmail.com>
2025-05-14 23:47:55 +0700

    Update database.py

--- src/database.py
+++ src/database.py
@@ -6,6 +6,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///recipes.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
