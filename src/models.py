adacbc0d399d5e05686d8a6c5217c0ccdb3af191
syber2013 <tralex2009@gmail.com>
2025-05-14 20:31:04 +0700

    Create models.py

---
+++ src/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from datetime import datetime
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///recipes.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+Base = declarative_base()
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    views = Column(Integer, default=0)
+    cooking_time = Column(Integer, default=0)
+    ingredients = Column(Text)
+    description = Column(Text)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+Base.metadata.create_all(bind=engine)
