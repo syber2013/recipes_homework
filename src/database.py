f0cc61b8bfd517b750bc04e8e95950720b3a32cf
syber2013 <tralex2009@gmail.com>
2025-05-14 20:29:40 +0700

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,39 +1,14 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from typing import List
-from database import get_db
-from models import Recipe
-from schemas import RecipeSchema, RecipeViewSchema
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-app = FastAPI()
+SQLALCHEMY_DATABASE_URL = "sqlite:///recipes.db"
 
-# GET /recipes — получить список всех рецептов
-@app.get("/recipes", response_model=List[RecipeViewSchema])
-def read_recipes(db: Session = Depends(get_db)):
-    recipes = db.query(Recipe).order_by(Recipe.views.desc(), Recipe.cooking_time.asc()).all()
-    return recipes
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# GET /recipes/{recipe_id} — получить детальную информацию о конкретном рецепте
-@app.get("/recipes/{recipe_id}", response_model=RecipeViewSchema)
-def read_recipe(recipe_id: int, db: Session = Depends(get_db)):
-    recipe = db.query(Recipe).filter(Recipe.id == recipe_id).first()
-    if recipe:
-        recipe.views += 1
-        db.commit()
-        return recipe
-    else:
-        return {"error": "Рецепт не найден"}
-
-# POST /recipes — создать новый рецепт
-@app.post("/recipes", response_model=RecipeViewSchema)
-def create_recipe(recipe: RecipeSchema, db: Session = Depends(get_db)):
-    db_recipe = Recipe(
-        name=recipe.name,
-        cooking_time=recipe.cooking_time,
-        ingredients=recipe.ingredients,
-        description=recipe.description,
-    )
-    db.add(db_recipe)
-    db.commit()
-    db.refresh(db_recipe)
-    return db_recipe
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
