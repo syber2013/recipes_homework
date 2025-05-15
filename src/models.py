dac198ba3c5d19be11a90d8a1b5f627040166f45
syber2013 <tralex2009@gmail.com>
2025-05-15 22:40:22 +0700

    Update models.py

--- src/models.py
+++ src/models.py
@@ -8,6 +8,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Base = declarative_base()
 
+
 class Recipe(Base):
     __tablename__ = "recipes"
 
@@ -19,5 +20,5 @@ class Recipe(Base):
     description = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-Base.metadata.create_all(bind=engine)
 
+Base.metadata.create_all(bind=engine)
